2fbbcdd9f1f2b4e4c1e83ad22249a2f2eb41c3df
slemonide <slemonide@example.com>
2020-12-16 23:47:38 -0800

    Complete test coverage; made tests run automatically on file change

--- README.md
+++ README.md
@@ -21,3 +21,28 @@ dashboard source code.
 To try production environment, use `docker-compose up --build prod`.
 
 Open http://localhost:8000/ to access locally running dash dashboard.
+
+## Unit testing
+
+To run tests, first switch to production version: `docker-compose up --build dev`.
+
+And then start tests container: `docker-compose up --build tests`.
+
+Tests will automatically re-run when you modify code or tests.
+
+These tests test api responses of dash backend. For that, you need
+to describe API request that dash makes to the backend. That is a simple
+piece of json. To get an idea what it looks like, use web inspector in
+your web browser.
+
+Right click on a web page and select "Inspect Element" or something similiar.
+Then select "Network". There, you would be able to see requests that web page
+makes. Try clicking some buttons to see if you can intercept some "POST" requests.
+
+Click on one and study "Request" and "Response".
+
+Also see tests provided for examples.
+
+Unfortunately, dash currently doesn't provide official documentation for this API
+so you have to learn it by observation.
+
--- tests/tests/test_api.py
+++ tests/tests/test_api.py
@@ -1,13 +1,28 @@
 import requests
+from copy import deepcopy
 
-url = "http://prod:8000"
+url = "http://dev:8000"
 api_url = url + "/_dash-update-component"
 
 ## Test Helpers
 
-def make_request(state):
-    pass
+def make_json_request(json):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Content-Type': 'application/json'
+    }
 
+    return requests.post(api_url, json=json, headers=headers)
+
+# Basic data state to make it easier to construct other states on top of
+base_json = {
+    "output": "store.data",
+    "outputs": { "id": "store", "property": "data" },
+    "inputs": [
+        { "id": "count", "property": "n_clicks"},
+        { "id": "clear", "property": "n_clicks"}
+    ],
+}
 
 ## Tests
 
@@ -15,47 +30,66 @@ def test_main_page_status_code_equals_200():
     response = requests.get(url)
     assert response.status_code == 200
 
-def test_count():
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Content-Type': 'application/json'
-    }
+def test_count_store_increment_none_state():
+    json = deepcopy(base_json)
+    json["changedPropIds"] = ["count.n_clicks"]
+    json["state"] = [{ "id":"store", "property": "data", "value": None }]
 
-    # click count button
-    json = {
-        "output": "store.data",
-        "outputs":
-            {
-                "id": "store",
-                "property": "data"
-            },
-        "inputs": [
-            {
-                "id": "count",
-                "property": "n_clicks", "value": 1
-            },
-            {
-                "id": "clear",
-                "property": "n_clicks"
-            }
-        ],
-        "changedPropIds": ["count.n_clicks"],
-        "state":
-        [{
-            "id":"store",
-            "property": "data",
-            "value": None
-        }]
-    }
-
-    response = requests.post(api_url, json=json, headers=headers)
+    response = make_json_request(json)
 
     expected_response = {
-        'response':
-            {
-                'store': {'data': {'clicks': 1}} # increment number of clicks!
-            },
-        'multi': True # meaning there's more callbacks comming
+        'response': { 'store': {'data': {'clicks': 1}} },
+        'multi': True
     }
     
+    assert response.json() == expected_response
+
+def helper_test_count_store_increment_non_null_state(value):
+    json = deepcopy(base_json)
+    json["changedPropIds"] = ["count.n_clicks"]
+    json["state"] = [{ "id":"store", "property": "data", "value": {"clicks": value} }]
+
+    response = make_json_request(json)
+
+    expected_response = {
+        'response': { 'store': {'data': {'clicks': value + 1}} },
+        'multi': True
+    }
+    
+    assert response.json() == expected_response
+
+def test_count_store_increment_non_null_state():
+  for value in range(1, 100):
+    helper_test_count_store_increment_non_null_state(value)
+
+def test_clear_count_store():
+    json = deepcopy(base_json)
+    json["changedPropIds"] = ["clear.n_clicks"]
+    json["state"] = [{ "id":"store", "property": "data", "value": {"clicks": 64} }]
+
+    response = make_json_request(json)
+
+    expected_response = {
+        'response': { 'store': {'data': {'clicks': 0}} },
+        'multi': True
+    }
+
+    assert response.json() == expected_response
+
+def test_render_store_data():
+    counter_data = 64
+
+    json = {}
+    json["changedPropIds"] = ["store.data"]
+    json["inputs"] = [{ "id": "store", "property": "data", "value": { "clicks": counter_data } }]
+    json["outputs"] = {"id": "output", "property": "children"}
+    json["output"] = "output.children"
+
+    response = make_json_request(json)
+
+    expected_response = {
+        'response': { 'output': {'children': "The count is " + str(counter_data) }},
+        'multi': True
+    }
+
     assert response.json() == expected_response
