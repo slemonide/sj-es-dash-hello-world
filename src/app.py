f7deb0be42828a511c9299919d4b89bc37c60c8a
fyrzbavqr <slemonide@gmail.com>
2020-12-21 21:19:22 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ app = dash.Dash(
 app.layout = html.Div([
     dcc.Store(id='store'),
 
-    html.Div("Hello world. I can auto update! One more test!"),
+    html.Div("Hello world. I can auto update! One more test! Sorry to bother you..."),
 
     html.Button('Count', id='count'),
     html.Button('Clear', id='clear'),
